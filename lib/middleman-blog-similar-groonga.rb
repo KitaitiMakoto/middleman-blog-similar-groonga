ef8a8077028afd6d86b78d7bb2723463ae0f5632
KITAITI Makoto <KitaitiMakoto@gmail.com>
2015-12-25 23:35:36 +0900

    Implement extension

--- lib/middleman-blog-similar-groonga.rb
+++ lib/middleman-blog-similar-groonga.rb
@@ -1,38 +1,63 @@
-# Require core library
 require 'middleman-core'
+require 'middleman-blog'
 
-# Extension namespace
-class MyExtension < ::Middleman::Extension
-  option :my_option, 'default', 'An example option'
+module Middleman::Blog
+  module Similar
+    class Groonga < ::Middleman::Extension
+      option :db_path, 'groonga/articles', 'Path to Groonga database'
 
-  def initialize(app, options_hash={}, &block)
-    # Call super to build options from the options_hash
-    super
+      def initialize(app, options_hash={}, &block)
+        super
 
-    # Require libraries only when activated
-    # require 'necessary/library'
+        app.config[:blog_similar_groonga] = self
 
-    # set up your extension
-    # puts options.my_option
+        @db_path = Pathname.new(options.db_path)
+        require 'groonga'
+      end
+
+      def after_configuration
+        if @db_path.file?
+          ::Groonga::Database.open @db_path.to_path
+        else
+          @db_path.dirname.mkpath unless @db_path.dirname.directory?
+          ::Groonga::Database.create(path: @db_path.to_path)
+          ::Groonga::Schema.create_table 'Articles', type: :hash do |table|
+            table.short_text 'title'
+            table.long_text 'body'
+            # TODO: tags
+          end
+          ::Groonga::Schema.create_table 'Terms', type: :patricia_trie, normalizer: :NormalizerAuto, default_tokenizer: 'TokenMecab'
+          ::Groonga::Schema.change_table 'Terms' do |table|
+            table.index 'Articles.body'
+          end
+        end
+
+        articles = ::Groonga['Articles']
+        app.sitemap.resources.each do |resource|
+          next unless resource.kind_of? ::Middleman::Blog::BlogArticle
+
+          key = resource.path
+          articles.add key unless articles[key]
+
+          articles[key].title = resource.title
+          articles[key].body = Nokogiri.HTML('<body>' + resource.body + '</body>').search('body').first.content
+        end
+      end
+
+      helpers do
+        def similar_articles
+          current_body = Nokogiri.HTML('<body>' + current_page.body + '</body>').search('body').first.content
+          results = ::Groonga['Articles'].select {|article|
+            article.body.similar_search(current_page.summary) &
+              (article.key != current_page.path)
+          }
+          results.sort([key: '_score', order: 'descending'])
+require 'pp';pp results
+          results
+        end
+      end
+    end
   end
-
-  def after_configuration
-    # Do something
-  end
-
-  # A Sitemap Manipulator
-  # def manipulate_resource_list(resources)
-  # end
-
-  # helpers do
-  #   def a_helper
-  #   end
-  # end
 end
 
-# Register extensions which can be activated
-# Make sure we have the version of Middleman we expect
-# Name param may be omited, it will default to underscored
-# version of class name
-
-# MyExtension.register(:my_extension)
+::Middleman::Extensions.register :blog_similar_groonga, ::Middleman::Blog::Similar::Groonga
